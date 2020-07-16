a7240cdd745ca0b296e8813025d5a684a237f5eb
Charuth Sudhakaran <charuthsudhakaran@gmail.com>
2020-07-16 21:54:24 +0530

    remove the render

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -64,8 +64,7 @@ class AuthorsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_author
-      author = Author.find(params[:id])
-      render json: {Author: author, Book: author.books }, status: :ok
+      @author = Author.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
